a2d5ca1fb720589989a8d1db1ac0fe9c5018413d
iumar <eminence742@gmail.com>
2026-04-17 14:44:15 +0100

    Add legacy browser support for mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'Android >= 5', 'Chrome >= 49', 'Safari >= 10', 'iOS >= 10'],
+      modernPolyfills: true,
+      renderLegacyChunks: true,
+    }),
+  ],
   server: {
     host: '127.0.0.1',
     port: 3000,
